9763d98855ebcc8512e266af77f2584c7f404f91
Aiko <aikootv@gmail.com>
2024-09-20 16:21:28 +0200

    Ajout gestion theme

--- hooks/useThemeColor.ts
+++ hooks/useThemeColor.ts
@@ -1,22 +1,18 @@
-/**
- * Learn more about light and dark modes:
- * https://docs.expo.dev/guides/color-schemes/
- */
+// Doc : https://docs.expo.dev/guides/color-schemes/
 
-import { useColorScheme } from 'react-native';
-
-import { Colors } from '@/constants/Colors';
+import { useColorScheme } from "react-native";
+import { Colors } from "@/constants/Colors"
 
 export function useThemeColor(
-  props: { light?: string; dark?: string },
-  colorName: keyof typeof Colors.light & keyof typeof Colors.dark
+    props: {light?: string; dark?: string}, //Permet de spé une couleur perso, laisser {} pour avoir la couleur par défault
+    colorName: keyof typeof Colors.light & keyof typeof Colors.dark //Permet d'aller target une ref précise dans Colors[dark ou light] (du genre background, text..)
 ) {
-  const theme = useColorScheme() ?? 'light';
-  const colorFromProps = props[theme];
+    const theme = useColorScheme() ?? 'light';
+    const colorFromProps = props[theme];    //Est ce qu'on a passé une couleur perso ?
 
-  if (colorFromProps) {
-    return colorFromProps;
-  } else {
-    return Colors[theme][colorName];
-  }
+    if (colorFromProps) {
+        return colorFromProps;              //Oui je la prend
+    } else {
+        return Colors[theme][colorName]     //Non je prend une couleur par défault en function du theme.
+    }
 }
