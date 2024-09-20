8fed623b75460edea6d2cb760fe670105f22a49b
Aiko <aikootv@gmail.com>
2024-09-20 16:34:20 +0200

    Setup index

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedView } from '@/components/ThemedView';
+import { ThemedText } from '@/components/ThemedText';
 
 export default function Index() {
   return (
     <SafeAreaView>
-        <Text>Your background color is </Text>
+        <ThemedView>
+          <ThemedText>Your background color is </ThemedText>
+        </ThemedView>
     </SafeAreaView>
   );
 }
